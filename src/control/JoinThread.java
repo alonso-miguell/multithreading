c3d2e58e91af4f9fd72eacb3bd9093694af9519d
Alonso Miguel <alonsomiguel.wpc@gmail.com>
2025-07-05 19:59:16 -0600

    Join and some thread control

---
+++ src/control/JoinThread.java
@@ -0,0 +1,75 @@
+package control;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JoinThread {
+    public static void main(String[] args) {
+        List<Long> inputNumbers = Arrays.asList(0L, 30L, 500L, 784265L);
+        List<FactorialThread> threads = new ArrayList<>();
+
+        inputNumbers.forEach(input -> {
+            FactorialThread factorialThread = new FactorialThread(input);
+
+            // Commenting this wait until last calculation is finished
+            factorialThread.setDaemon(true);
+            threads.add(factorialThread);
+        });
+
+        threads.forEach(Thread::start);
+
+        threads.forEach(thread -> {
+
+            // Needs to use join after starting the thread
+            // Join will wait for all thread during that time before returning to mainThread
+            // This is useful for waiting at most X time for other threads before continuing in main
+            try {
+                thread.join(2000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (!thread.isAlive()) {
+                System.out.println(thread.getName() + " has already finished after joining");
+            }
+        });
+
+        threads.forEach(thread -> {
+            if (thread.isFinished) {
+                System.out.println("Calculation for " + thread.inputNumber + " has been completed: " + thread.result);
+            } else {
+                System.out.println("Calculation for " + thread.inputNumber + " IN PROGRESS");
+            }
+        });
+    }
+
+    static class FactorialThread extends Thread {
+        long inputNumber;
+        public boolean isFinished = false;
+        public BigInteger result = BigInteger.ZERO;
+
+        FactorialThread(long inputNumber) {
+            this.inputNumber = inputNumber;
+        }
+
+        @Override
+        public void run() {
+            this.result = calculateFactorial(this.inputNumber);
+
+            this.isFinished = true;
+        }
+
+        BigInteger calculateFactorial(long inputNumber) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = 1; i <= inputNumber; i++) {
+                tempResult = tempResult.multiply(new BigInteger(String.valueOf(i)));
+
+            }
+
+            return tempResult;
+        }
+    }
+}
---
+++ src/control/ThreadLimit.java
@@ -0,0 +1,39 @@
+package control;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/*
+This class prints the limit of Threads that can be created by the CPU
+before throwing a memory exception
+ */
+
+
+/* Current result for 7800X3D, took around 10 mins
+
+161576
+#
+# There is insufficient memory for the Java Runtime Environment to continue.
+# Native memory allocation (malloc) failed to allocate 1292720 bytes. Error detail: AllocateHeap
+# An error report file with more information is saved as:
+# F:\Home\Cursos\JAVA spring\multithreading\hs_err_pid17612.log
+[280.758s][warning][os] Loading hsdis library failed
+
+Process finished with exit code 1
+ */
+
+public class ThreadLimit {
+    public static void main(String [] args) {
+        AtomicInteger counter = new AtomicInteger(0);
+        while(true){
+            new Thread(() -> {
+                System.out.println(counter.incrementAndGet());
+                try {
+                    Thread.sleep(Duration.ofHours(1).toMillis());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+} 
